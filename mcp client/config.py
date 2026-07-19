6bccb08a5f645f9281836efac6c2d95c29b151bc
Asif Developer <130160475+Asif9916@users.noreply.github.com>
2026-07-19 13:33:34 +0530

    Update config.py

--- mcp client/config.py
+++ mcp client/config.py
@@ -1,12 +1,3 @@
-# import os
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# MCP_SERVER_URL = os.getenv("MCP_SERVER_URL")
-# ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
-# MODEL = "claude-sonnet-4-5"
-
 import os
 from dotenv import load_dotenv
 
